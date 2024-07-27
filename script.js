def75d88366f68bf6647be494dcf2efeac44be8d
uttesh <uttesh@gmail.com>
2024-07-27 18:13:01 +0530

    updated the spinner

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 async function fetchData() {
   const sheetId = "1YVu3PszcFrC9-arFm1qcy5XSmNi-orj7U6UONG2Boz4"; // Replace with your actual sheet ID
   const range = "heartbeat!A:C"; // Replace with your actual range
-  const apiKey = "AIzaSyC-cDc5zk9gTtvgMbkz9OnWFmZGN8Iqkgg"; // Replace with your API key if needed
+  const pulsebot = "AIzaSyC-cDc5zk9gTtvgMbkz9OnWFmZGN8Iqkgg"; // Replace with your API key if needed
 
   const response = await fetch(
-    `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`
+    `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${pulsebot}`
   );
   const data = await response.json();
-  console.log("data ww::", data);
   return data.values.slice(1).map((row) => ({
     date: row[0],
     device: row[1],
@@ -17,7 +16,24 @@ async function fetchData() {
 
 async function drawChart() {
   const data = await fetchData();
+  updateCountdown(30, data[0]["status"], data[0]["date"]); // 30 seconds countdown
   console.log(data[0]["status"]);
 }
 
+function updateCountdown(seconds, status, time) {
+  const statusText = document.getElementById("status-text");
+
+  let remainingTime = seconds;
+
+  const interval = setInterval(() => {
+    if (remainingTime <= 0) {
+      clearInterval(interval);
+      statusText.textContent = `DG is ${status}: (${time})`;
+      drawChart();
+    } else {
+      statusText.textContent = `DG is ${status}: ${remainingTime}s`;
+      remainingTime--;
+    }
+  }, 1000);
+}
 drawChart();
